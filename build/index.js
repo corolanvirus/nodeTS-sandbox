142ea3c18de96810168afa47c69ecf8987f87f5f
corolanvirus <contact@yanis-ouali.fr>
2022-04-07 18:05:23 +0200

    add cluster

--- build/index.js
+++ build/index.js
@@ -1,3 +1,30 @@
 "use strict";
-console.log('Hello world!');
-console.log('slt');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var cluster_1 = __importDefault(require("cluster"));
+var http_1 = __importDefault(require("http"));
+var os_1 = require("os");
+var process_1 = __importDefault(require("process"));
+var numCPUs = os_1.cpus().length;
+if (cluster_1.default.isMaster) {
+    console.log("Primary " + process_1.default.pid + " is running");
+    console.log("Number of CPUs is " + numCPUs);
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster_1.default.fork();
+    }
+    cluster_1.default.on('exit', function (worker, code, signal) {
+        console.log("worker " + worker.process.pid + " died");
+    });
+}
+else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    http_1.default.createServer(function (req, res) {
+        res.writeHead(200);
+        res.end('hello world\n');
+    }).listen(8000);
+    console.log("Worker " + process_1.default.pid + " started, listening on port 8000");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-console.log('Hello world!');
-console.log('slt');
+import cluster from 'cluster';
+import http from 'http';
+import { cpus } from 'os';
+import process from 'process';
+
+const numCPUs = cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Primary ${process.pid} is running`);
+  console.log(`Number of CPUs is ${numCPUs}`);
+
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(8000);
+
+  console.log(`Worker ${process.pid} started, listening on port 8000`);
+}
